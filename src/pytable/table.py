ad0c8ab4112e3d8907925ab8e73de54f81b9773a
Genadij Razdorov <genadijrazdorov@gmail.com>
2023-09-20 10:13:45 +0200

    feat: add Table as sequence-of-rows implementation
    close #7, close #9

--- src/pytable/table.py
+++ src/pytable/table.py
@@ -1,3 +1,32 @@
-class Table:
-    def __init__(self, *args, **kwargs):
-        pass
+from . import abc
+
+
+class Table(abc.Table):
+    columns = tuple()
+    
+    def __new__(cls, sequence_of_rows: list[tuple] = None, columns: list[str] = None):
+        obj = super().__new__(cls)
+        if sequence_of_rows is None:
+            sequence_of_rows = []
+        obj._tbl = sequence_of_rows
+        if columns is None:
+            columns = tuple()
+        else:
+            columns = tuple(columns)
+        
+        obj.columns = columns
+        return obj
+
+    def __getitem__(self, index):
+        index, col = super().__getitem__(index)
+            
+        if isinstance(index, slice):
+            if isinstance(col, slice):
+                return Table([row[col] for row in self._tbl[index]], self.columns[col])
+            else:
+                return tuple(row[col] for row in self._tbl[index])
+        else:
+            return self._tbl[index][col]
+    
+    def __len__(self):
+        return len(self._tbl)
