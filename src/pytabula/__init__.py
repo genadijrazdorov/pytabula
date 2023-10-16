0b964e2cd5a614a672b1d8d0497bfaaae3387472
Genadij Razdorov <genadijrazdorov@gmail.com>
2023-10-16 10:28:27 +0200

    feat: add poetry-dynamic-versioning
    close #50

--- src/pytabula/__init__.py
+++ src/pytabula/__init__.py
@@ -14,3 +14,6 @@ __pytabula__ = """
 """.strip().splitlines()
 
 __pytabula__ = Table([row.split() for row in __pytabula__], columns="pytabula")
+
+__version__ = "0.0.0"
+__version_tuple__ = (0, 0, 0)
